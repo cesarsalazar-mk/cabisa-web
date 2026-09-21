e89c27f1760adcd6d827883b44b8ee29c66784ca
luisdeleon-mk <ledr1993@gmail.com>
2026-09-20 21:35:49 -0600

    update web

--- src/pages/reports/reportDocuments/components/reportDocumentTable.js
+++ src/pages/reports/reportDocuments/components/reportDocumentTable.js
@@ -223,6 +223,20 @@ function ReportDocumentTable(props) {
       width: 100,
       render: text => <Tag type='documentStatus' value={text} />,
     },
+    {
+      title: 'Estado de pago',
+      dataIndex: 'payment_status',
+      key: 'payment_status',
+      width: 150,
+      render: text =>
+        text === 'PAID' ? (
+          <AntTag color='green'>Pagada</AntTag>
+        ) : text === 'UNPAID' ? (
+          <AntTag color='orange'>Pendiente de pago</AntTag>
+        ) : (
+          '-'
+        ),
+    },
   ]
 
   const { summary } = props
@@ -350,15 +364,26 @@ function ReportDocumentTable(props) {
             </Select>
           </Col>
           <Col xs={24} sm={12} md={5} lg={5}>
-            <Search
-              key={`total-invoice-${props.filtersResetKey}`}
-              type='tel'
-              prefix={<SearchOutlined className={'cabisa-table-search-icon'} />}
-              placeholder='Total'
-              className={'cabisa-table-search customSearch'}
-              onSearch={props.handleFiltersChange('totalInvoice')}
+            <Select
+              key={`payment-status-${props.filtersResetKey}`}
+              className={'single-select'}
+              placeholder={'Estado de pago'}
               size={'large'}
-            />
+              style={{ width: '100%', height: '40px' }}
+              getPopupContainer={trigger => trigger.parentNode}
+              onChange={props.handleFiltersChange('paymentStatus')}
+              value={props.filters?.paymentStatus ?? ''}
+            >
+              <Option value={''}>
+                <AntTag color='gray'>Todo</AntTag>
+              </Option>
+              <Option value={'PAID'}>
+                <AntTag color='green'>Pagada</AntTag>
+              </Option>
+              <Option value={'UNPAID'}>
+                <AntTag color='orange'>Pendiente de pago</AntTag>
+              </Option>
+            </Select>
           </Col>
         </Row>
       </div>
